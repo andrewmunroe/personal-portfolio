00c1c3bea5a83a2baf03c880585920e154f7d75b
Andrew Munroe <andrewemunroe@gmail.com>
2022-05-17 10:47:14 -0700

    New design

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+const controls = document.querySelectorAll('.controls');
+const body = document.body;
+let emphasisElements = document.querySelectorAll('main p, main h1, main h3');
+
+controls.forEach(control => control.addEventListener('click', event => {
+  const target = event.target;
+  changeDisplayMode(target);
+}));
+
+function changeDisplayMode(target) {
+  if (target.classList.contains('active')) {
+    return;
+  }
+  else {
+    setData(target.value);
+    // set inactive/active state on relevant control buttons
+    let parent = target.parentNode;
+    let parentButtons = parent.querySelectorAll('.controls');
+    parentButtons.forEach(button => button.classList.remove('active'));
+    target.classList.add('active');
+  }
+}
+
+function setData(data) {
+  if (body.classList.contains('emphatic')) {
+    emphasisElements.forEach(element => {
+      element.tagName === 'H3' ? element.querySelector('a').innerText = element.querySelector('a').innerText.replace('!!', '') : element.innerText = element.innerText.replaceAll('!!', '.');
+    })
+  }
+  body.removeAttribute('class');
+  if (data === 'bad') { body.classList.add('fancy'); }
+  else if (data === 'fancy') { body.classList.add('fancy'); }
+  else if (data === 'emphatic') {
+    body.classList.add('emphatic');
+    emphatisize();
+  }
+}
+
+function emphatisize() {
+  body.classList.add('emphatic');
+  emphasisElements.forEach(element => {
+    element.tagName === 'H3' ? element.querySelector('a').innerText += '!!' : element.innerText = element.innerText.replaceAll('.', '!!');
+  });
+}
